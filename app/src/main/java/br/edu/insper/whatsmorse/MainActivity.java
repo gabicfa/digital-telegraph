7662cabaa3654be8299c6b8d5750c48789b277d0
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-16 17:47:18 -0300

    Limpei o programa anterior, agora so tem um botão centralizado que envia um toast quando pressionado

--- app/src/main/java/br/edu/insper/whatsmorse/MainActivity.java
+++ app/src/main/java/br/edu/insper/whatsmorse/MainActivity.java
@@ -21,32 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Log.i("MainActivity", "VAPE NATION Y'ALL!");
 
-        final EditText campo = (EditText) findViewById(R.id.campo);
         Button botaoEnviar = (Button) findViewById(R.id.botaoEnviar);
 
         assert botaoEnviar != null;
         botaoEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                assert campo != null;
-//                Log.i("MainActivity", campo.getText().toString());
-                Toast.makeText(MainActivity.this, campo.getText().toString(), Toast.LENGTH_LONG).show();
-                campo.setText("");
-                campo.setHint("Digite outro algo, seu burro");
+                Toast.makeText(MainActivity.this, "Parabéns, você apertou o botão", Toast.LENGTH_LONG).show();
 
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
