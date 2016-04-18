109a3d561b64fe89d0542264485ab31a11f1a503
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-18 08:28:21 -0300

    Criei um teste para cada 'nível' da árvore, por enquanto vou deixar por isso, vou partir para a implementação

--- app/src/test/java/br/edu/insper/whatsmorse/ExampleUnitTest.java
+++ app/src/test/java/br/edu/insper/whatsmorse/ExampleUnitTest.java
@@ -27,6 +27,33 @@ public class ExampleUnitTest {
         assertEquals(letra, 'n');
     }
 
+    @Test
+    public void charIsR() {
+        SelectedCharacter selectedCharacter = new SelectedCharacter();
+        selectedCharacter.setValues(false, true, false, null);
+        char letra = selectedCharacter.getValue();
+
+        assertEquals(letra, 'r');
+    }
+
+    @Test
+    public void charIsH() {
+        SelectedCharacter selectedCharacter = new SelectedCharacter();
+        selectedCharacter.setValues(false, false, false, false, null);
+        char letra = selectedCharacter.getValue();
+
+        assertEquals(letra, 'r');
+    }
+
+    @Test
+    public void charIs9() {
+        SelectedCharacter selectedCharacter = new SelectedCharacter();
+        selectedCharacter.setValues(true, true, true, true, false, null);
+        char letra = selectedCharacter.getValue();
+
+        assertEquals(letra, '9');
+    }
+
 //    @Test
 //    public void addition_isCorrect() throws Exception {
 //        assertEquals(4, 2 + 2);
