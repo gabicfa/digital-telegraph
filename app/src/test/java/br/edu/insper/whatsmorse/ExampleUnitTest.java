14405b3ae5a604a9dd6bf8531cf8a0dce21cce5b
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-18 08:23:36 -0300

    Estou criando alguns testes para começar a programar a árvore, entender como usar o JUnit no android studio levou mais tempo do que eu gostaria...

--- app/src/test/java/br/edu/insper/whatsmorse/ExampleUnitTest.java
+++ app/src/test/java/br/edu/insper/whatsmorse/ExampleUnitTest.java
@@ -8,8 +8,27 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void charIsE() {
+        SelectedCharacter selectedCharacter = new SelectedCharacter();
+        selectedCharacter.setValues(false, null);
+        char letra = selectedCharacter.getValue();
+
+        assertEquals(letra, 'e');
     }
+
+    @Test
+    public void charIsN() {
+        SelectedCharacter selectedCharacter = new SelectedCharacter();
+        selectedCharacter.setValues(true, false, null);
+        char letra = selectedCharacter.getValue();
+
+        assertEquals(letra, 'n');
+    }
+
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
 }
