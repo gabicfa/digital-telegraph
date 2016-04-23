9869228d4b85349ee36f52cf0e9897b6f35f7943
gabicfa <gabrielacfa@al.insper.edu.br>
2016-04-23 19:48:48 -0300

    Finalização da sprint 2

--- app/src/main/java/br/edu/insper/whatsmorse/MainActivity.java
+++ app/src/main/java/br/edu/insper/whatsmorse/MainActivity.java
@@ -12,43 +12,68 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import br.edu.insper.whatsmorse.model.SelectedCharacter;
+
 public class MainActivity extends AppCompatActivity {
 
     boolean aperto;
     public static List<Boolean> listaDeApertos = new ArrayList<Boolean>();
+    private TextView mensagem;
+    private String frase = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         Button botaoEnviar = (Button) findViewById(R.id.botaoEnviar);
+        this.mensagem = (TextView) findViewById(R.id.textView);
 
         assert botaoEnviar != null;
+
+        final CountDownTimer espaco = new CountDownTimer(4000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                frase += " ";
+            }
+        }.start();
 
         final CountDownTimer remainingTimeCounter = new CountDownTimer(2000, 1000) {
             public void onTick(long millisUntilFinished) {
             }
 
             public void onFinish() {
-                Toast.makeText(MainActivity.this, "Parabéns, você fez um espaco", Toast.LENGTH_SHORT).show();
                 System.out.println(listaDeApertos);
-                listaDeApertos.clear();
+                SelectedCharacter selectedChar = new SelectedCharacter();
+                selectedChar.setNode(listaDeApertos);
+
+                if (listaDeApertos.size() > 0) {
+                    char letra = selectedChar.getValue();
+                    System.out.println(letra);
+                    frase += letra;
+                    System.out.println(frase);
+                    mensagem.setText(frase);
+                    listaDeApertos.clear();
                 }
+            }
         }.start();
 
         botaoEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Parabéns, você fez um ponto", Toast.LENGTH_SHORT).show();
                 aperto= false;
                 if (listaDeApertos.size()<5){
                     listaDeApertos.add(aperto);
@@ -56,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(listaDeApertos);
                 remainingTimeCounter.cancel();
                 remainingTimeCounter.start();
+                espaco.cancel();
+                espaco.start();
             }
         });
 
@@ -64,14 +91,15 @@ public class MainActivity extends AppCompatActivity {
         botaoEnviar.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Toast.makeText(MainActivity.this, "Parabéns, você fez um traço", Toast.LENGTH_SHORT).show();
                 aperto = true;
                 if (listaDeApertos.size()<5){
                     listaDeApertos.add(aperto);
                 }
                 System.out.println(listaDeApertos);
                 remainingTimeCounter.cancel();
-                remainingTimeCounter.start();
+                    remainingTimeCounter.start();
+                espaco.cancel();
+                espaco.start();
                 return true;
             }
         });
