bda7b401fb650304fdd1250b36426d29c47cc726
gabicfa <gabrielacfa@al.insper.edu.br>
2016-05-01 17:46:47 -0300

    enviar mensagem certo

--- app/src/main/java/br/edu/insper/whatsmorse/NurseActivity.java
+++ app/src/main/java/br/edu/insper/whatsmorse/NurseActivity.java
@@ -9,7 +9,14 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.insper.whatsmorse.model.*;
+
 public class NurseActivity extends AppCompatActivity {
+    private Node[] nodes;
+    private NodeFactory nodeFactory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +27,20 @@ public class NurseActivity extends AppCompatActivity {
         //      correspondentes às letras
         //      TODO: Completar esta lísta (ou montar outra estrutura de dados conforme achar necessário) utilizando a árvore para criar a "cola do ajudante"
 
-        String[] charCodes = {"a", "b", "c"};
+        //  PC - Pegamos valor à valor dos chars na lista dos nós
+        nodeFactory = new NodeFactory();
+        nodes = nodeFactory.getNodes();
+        List<Character> charCodes =  new ArrayList<>();
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, charCodes);
+        for(Node node : nodes) {
+            if(node.getLetra() != 0) {
+                charCodes.add(node.getLetra());
+            }
+        }
+
+//        String[] charCodes = {"a", "b", "c"};
+
+        ArrayAdapter<Character> myAdapter = new ArrayAdapter<Character>(this, android.R.layout.simple_list_item_1, charCodes);
 
         ListView charList = (ListView) findViewById(R.id.listView);
 
