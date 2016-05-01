0190c7b1d48d1ce6ef9fe4fa496700179546f72b
gabicfa <gabrielacfa@al.insper.edu.br>
2016-04-30 23:49:09 -0300

    Adicionei os toques de código morse para o numero

--- app/src/main/java/br/edu/insper/whatsmorse/NumberEntryActivity.java
+++ app/src/main/java/br/edu/insper/whatsmorse/NumberEntryActivity.java
@@ -1,13 +1,25 @@
 package br.edu.insper.whatsmorse;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import br.edu.insper.whatsmorse.model.SelectedCharacter;
+
 public class NumberEntryActivity extends AppCompatActivity {
-    private String numero;
+    private String telefone = "";
+    boolean aperto;
     private EditText editTextNumero;
+    public static List<Boolean> listaDeApertos = new ArrayList<Boolean>();
 
 
 
@@ -21,6 +33,75 @@ public class NumberEntryActivity extends AppCompatActivity {
         Button botaoNumero = (Button) findViewById(R.id.botao_toque_numero);
         this.editTextNumero = (EditText) findViewById(R.id.edit_text_numero);
 
+        assert botaoEnviar != null;
+        assert botaoBackspace != null;
+        assert botaoNumero != null;
+
+
+        final CountDownTimer mudancaDeNumero = new CountDownTimer(1500, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                System.out.println(listaDeApertos);
+                SelectedCharacter selectedChar = new SelectedCharacter();
+                selectedChar.setNode(listaDeApertos);
+
+                if (listaDeApertos.size() == 5) {
+                    char numero = selectedChar.getValue();
+                    System.out.println(numero);
+                    telefone += numero;
+                    System.out.println(telefone);
+                    editTextNumero.setText(telefone);
+                    editTextNumero.setSelection(editTextNumero.getText().length());
+                    listaDeApertos.clear();
+                }
+            }
+        }.start();
+
+        botaoNumero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aperto= false;
+                if (listaDeApertos.size()<5){
+                    listaDeApertos.add(aperto);
+                }
+                System.out.println(listaDeApertos);
+                mudancaDeNumero.cancel();
+                mudancaDeNumero.start();
+            }
+        });
+
+        botaoNumero.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                aperto = true;
+                if (listaDeApertos.size()<5){
+                    listaDeApertos.add(aperto);
+                }
+                System.out.println(listaDeApertos);
+                mudancaDeNumero.cancel();
+                mudancaDeNumero.start();
+                return true;
+            }
+        });
+
+        botaoBackspace.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                if (!Objects.equals(telefone, "")) {
+                    telefone = telefone.substring(0, telefone.length() - 1);
+                    editTextNumero.setText(telefone);
+                    editTextNumero.setSelection(editTextNumero.getText().length());       // Para o cursor seguir a mensagem
+                    mudancaDeNumero.cancel();
+                    mudancaDeNumero.start();
+                }
+            }
+        });
 
     }
+
+
 }
